70f03adaf89543d597ff9173d78c7aae483716eb
masahibi <masahibi13@gmail.com>
2021-04-08 16:37:23 +0900

    commit

---
+++ cmake-build-debug/CMakeFiles/C_C__.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C_C__.dir/main.cpp.o"
+  "C_C__"
+  "C_C__.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C_C__.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
